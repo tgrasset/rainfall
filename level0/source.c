bae130bba9d8b195c2dedbd41388c52f9c72da06
Thibaut GRASSET <tgrasset@bess-f3r6s6.clusters.42paris.fr>
2024-01-09 18:11:23 +0100

    level0 finito

--- level0/source.c
+++ level0/source.c
@@ -6,18 +6,20 @@
 
 int main(int argc, char **argv)
 {
-    char strcpy;
+ 
+    char *arg[2];
     gid_t id;
     uid_t uid;
 
     if (atoi(argv[1]) == 423) //0x1a7
     {
-        strcpy = strdup("/bin/sh");
+        arg[0] = strdup("/bin/sh");
+        arg[1] = NULL;
         id = getegid();
         uid = geteuid();
         setresgid(id, id, id);
         setresuid(uid, uid, uid);
-        execve("/bin/sh", &strcpy, NULL);
+        execv("/bin/sh", arg);
     }
     else 
         fwrite("No !\n", 1, 5, stderr);

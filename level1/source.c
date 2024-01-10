60ab8b3831b7d2d0b477269ca16b4ac11fe7eb3f
Thibaut GRASSET <tgrasset@bess-f1r1s8.clusters.42paris.fr>
2024-01-10 16:37:21 +0100

    fixed

--- level1/source.c
+++ level1/source.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 
 void run(void) {
-    fwrite("Good... Wait what?", 1, 19, stdout);
+    fwrite("Good... Wait what?\n", 1, 19, stdout);
     system("/bin/sh");
 }
 
 int main(void) {
-    char buffer[76];
+    char buffer[64];
     gets(buffer);
-    return (0);
 }

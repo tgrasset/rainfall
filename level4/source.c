aea4a48fb66cb7e1c31d5dc33387f152d00dcb07
Thibaut GRASSET <tgrasset@bess-f3r3s3.clusters.42paris.fr>
2024-01-15 15:16:11 +0100

    level4 done

---
+++ level4/source.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int m;
+
+void p(char *buffer)
+{
+    printf(buffer);
+}
+
+
+void n()
+{
+    char buffer[512];
+
+    fgets(buffer, 512, stdin);
+    p(buffer);
+    if (m == 16930116)
+    {
+        system("/bin/cat /home/user/level5/.pass");
+    }
+}
+
+int main()
+{
+    n();
+}

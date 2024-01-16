bc25c38c5e658ccd0caa967c80a30204ed112234
Thibaut GRASSET <tgrasset@bess-f3r6s4.clusters.42paris.fr>
2024-01-16 10:32:52 +0100

    level6 done

---
+++ level6/source.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdlib.h>
+
+void n() {
+    system("/bin/cat /home/user/level7/.pass");
+}
+
+void m() {
+    puts("Nope");
+}
+
+int main(int ac, char **av) {
+    char *buffer;
+    void (**f)(void);
+
+    buffer = (char *)malloc(64);
+    f = (void (**)(void))malloc(4);
+
+    *f = m;
+    strcpy(buffer, av[1]);
+    (*f)();
+}
